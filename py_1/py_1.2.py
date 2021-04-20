7ef38ae1d9c95317bd6f1b68e2502429651f1bef
kyrylo <data.mail.wild@gmail.com>
2021-04-20 18:29:51 +0200

    first program:)

---
+++ py_1/py_1.2.py
@@ -0,0 +1,6 @@
+#first program:)
+# 3 groups of kids, and everybody need 2 icecream
+first_group = int(input(" Input kids in 1 group : "))
+second_group = int(input(" Input kids in 2 group : "))
+third_group = int(input(" Input kids in 3 group : "))
+print((first_group + second_group + third_group)*2)
