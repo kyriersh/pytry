7e0baa52e70832d5b883bca7d0bec3700decbbb3
kyrylo <data.mail.wild@gmail.com>
2021-04-22 22:06:05 +0200

    IF logic block_1.2

---
+++ py_2/py_2.7.py
@@ -0,0 +1,8 @@
+# right-angled or not right-angled
+side_1 = int(input())
+side_2 = int(input())
+side_3 = int(input())
+if side_1**2 + side_2**2 == side_3**2 :
+    print("right-angled")
+else:
+    print("not right-angled")
