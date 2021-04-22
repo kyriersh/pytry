5c8c66fa2b25368e3ecf379c4d211692f8954bf1
kyrylo <data.mail.wild@gmail.com>
2021-04-22 21:53:55 +0200

    IF logic block_1.1

---
+++ py_2/py_2.6.py
@@ -0,0 +1,7 @@
+# age-control in night club
+name = str(input( " Name : " ))
+age = int(input( " Age : " ))
+if age >= 18 :
+    print(name + " Welcome ! ")
+else:
+    print(name +  " Enter denied !")
