c214364a9094a889d58496d08a861c05063c984e
kyrylo <data.mail.wild@gmail.com>
2021-04-23 21:42:32 +0200

    boolean operators AND OR NOT

---
+++ py_2/py_2.9.1.py
@@ -0,0 +1,9 @@
+# program to check open or not
+day = int(input(" day : "))
+hour = int(input(" hour : "))
+if 5>= day !=0 and 8 <= hour <= 20 :
+    print(" open ")
+elif day == 0 :
+    print("wrong day")
+else:
+    print("closed")
