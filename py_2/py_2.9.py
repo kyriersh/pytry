60b31a88682caffbe88ee6e0d7b79c1adb3adbff
kyrylo <data.mail.wild@gmail.com>
2021-04-23 20:07:09 +0200

    ala fizz buzz

---
+++ py_2/py_2.9.py
@@ -0,0 +1,11 @@
+# prog liki fizz buzz
+num = int(input(" Input number : "))
+if num % 2 == 0 :
+    print(num * 2)
+elif num % 3 == 0 :
+    print(num * 3)
+elif num == 0 :
+    print(num)
+else:
+    print(num)
+
