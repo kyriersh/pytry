bb3e626a28fc1853675ecdd81d0249623f713874
kyrylo <data.mail.wild@gmail.com>
2021-04-23 21:52:25 +0200

    boolean operators AND OR NOT # 1.1

---
+++ py_2/py_2.9.2.py
@@ -0,0 +1,10 @@
+age = int(input(" age : "))
+if 0 < age <= 11 :
+    print("child")
+elif 12 <= age <= 17 :
+    print("teen")
+elif 18 <= age <= 64 :
+    print("adult")
+else:
+    print("senior")
+
