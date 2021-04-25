1dd0f35ad31677dec3b33816ce44917c099dd3f1
kyrylo <data.mail.wild@gmail.com>
2021-04-25 18:03:43 +0200

    practice with dict, just another one v1.3

--- py_2/py_2.9.3.py
+++ py_2/py_2.9.3.py
@@ -1,12 +1,12 @@
-# cost of semestr / average ball
+# cost of semester / average ball
 name = input(" enter you name : ")
 sem1_score = int(input(" 1 sem score : "))
 sem2_score = int(input(" 2 sem score : "))
-if 90 <= ((sem1_score + sem2_score) / 2) <= 100 :
+if 90 <= ((sem1_score + sem2_score) / 2) <= 100:
     print(50)
-elif 80 <= ((sem1_score + sem2_score) / 2) <= 89 :
+elif 80 <= ((sem1_score + sem2_score) / 2) <= 89:
     print(30)
-elif 70 <= ((sem1_score + sem2_score) / 2) <= 79 :
+elif 70 <= ((sem1_score + sem2_score) / 2) <= 79:
     print(10)
-elif 0 <= ((sem1_score + sem2_score) / 2) <= 69 :
+elif 0 <= ((sem1_score + sem2_score) / 2) <= 69:
     print(0)
