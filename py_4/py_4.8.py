9dfa2fe1b2ef6786a816135887e3c927b4185db0
kyrylo <data.mail.wild@gmail.com>
2021-04-27 15:17:36 +0200

    # practice with modules v1.1

---
+++ py_4/py_4.8.py
@@ -0,0 +1,14 @@
+#  practice with modules
+from math import pi
+radius = int(input(" input radius : "))
+def perim():
+    print("Perimetr:",2*pi*radius)
+    return
+def area():
+    print("Area:",pi*radius**2)
+    return
+perim()
+area()
+
+
+
