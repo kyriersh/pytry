5b025e3ed7a14ea3e4297d2e4b8f80a55e0bf673
kyrylo <data.mail.wild@gmail.com>
2021-04-27 08:27:02 +0200

    # fizz buzz modificated(step 2)

--- py_3/py_3.9.4.py
+++ py_3/py_3.9.4.py
@@ -1,11 +1,14 @@
 # fizz buzz
 a = int(input())
-for x in range(1, a):
+for x in range(1, a, 2):
     if x % 3 == 0 and x % 5 == 0:
         print("solo learn")
+        continue
     elif x % 3 == 0:
         print("solo")
+        continue
     elif x % 5 == 0 :
         print("learn")
-    elif x % 2 != 0:
+        continue
+    else:
         print(x)
