7a05f4464abc268ba74c3a96960e98ac09768c6d
kyrylo <data.mail.wild@gmail.com>
2021-04-27 08:43:58 +0200

    # my function "def" 1.1

--- py_4/py_4.1.py
+++ py_4/py_4.1.py
@@ -1,10 +1,16 @@
 # my function "def"
-a = int(input())
-b = int(input())
+a = int(input(" input 1 : "))
+b = int(input(" input 2 : "))
+
 def my_func1():
+
     for x in range(a, b, 1):
         if x % 2 == 0:
             print("qwerty")
         else:
             print(x)
 my_func1()
+user_name = str(input(" name : "))
+def name_welcome():
+    print(user_name + " Welcome !")
+name_welcome()
