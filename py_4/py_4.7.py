2087420bb978e2997c595837fcd07f9c13e85aca
kyrylo <data.mail.wild@gmail.com>
2021-04-27 15:01:13 +0200

    # practice with modules

---
+++ py_4/py_4.7.py
@@ -0,0 +1,15 @@
+# practice with modules
+import random
+for i in range(5):
+    value = random.randint(1,6)
+    print(value)
+import random
+random.seed(int(input()))
+
+#generate the random values for every dice
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+
+print(dice1)
+print(dice2)
+
