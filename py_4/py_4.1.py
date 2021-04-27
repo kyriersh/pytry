e25d1bbf5d2c1ba9f61b7745fda1c25ffe21dc0d
kyrylo <data.mail.wild@gmail.com>
2021-04-27 08:37:32 +0200

    # my function "def"

---
+++ py_4/py_4.1.py
@@ -0,0 +1,10 @@
+# my function "def"
+a = int(input())
+b = int(input())
+def my_func1():
+    for x in range(a, b, 1):
+        if x % 2 == 0:
+            print("qwerty")
+        else:
+            print(x)
+my_func1()
