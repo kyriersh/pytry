e1bdae65f1415bbbf99cdfc3626ce79cfd3c1a96
kyrylo <data.mail.wild@gmail.com>
2021-04-28 15:57:48 +0200

    # testing read file v 1.1

--- py_5/py_5.2.py
+++ py_5/py_5.2.py
@@ -1,5 +1,8 @@
-# testing read file
+# testing read file v 1.1
 file = open("/home/kyrylo/Desktop/qwerty.txt", 'r')
 cont = file.read()
 print(cont)
+print(len(cont))
+for line in cont:
+    print(line)
 file.close()
