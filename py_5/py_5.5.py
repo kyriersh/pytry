4631347d37594beed97a31658d8185a8a0656d2a
kyrylo <data.mail.wild@gmail.com>
2021-05-01 17:45:56 +0200

    # write/read v1.2

---
+++ py_5/py_5.5.py
@@ -0,0 +1,14 @@
+file = open("/home/kyrylo/Desktop/qwerty.txt", "r")
+
+#your code goes here
+fileLines = []
+for lines in file:
+	fileLines.append(lines)
+length = 1
+for n in fileLines:
+	if length == len(fileLines):
+		print(n[0] + str(len(n)))
+	else:
+		print(n[0] + str(len(n)-1))
+	length += 1
+file.close()
