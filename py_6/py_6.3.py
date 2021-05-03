b634e206e0fcd5946446699e7d2a5664c1c8997d
kyrylo <data.mail.wild@gmail.com>
2021-05-03 19:55:59 +0200

    # practice with dict use ".get "

---
+++ py_6/py_6.3.py
@@ -0,0 +1,12 @@
+# practice with dict use ".get "
+books = {
+    "Life of Pi": "Adventure Fiction",
+    "The Three Musketeers": "Historical Adventure",
+    "Watchmen": "Comics",
+    "Bird Box": "Horror",
+    "Harry Potter":"Fantasy Fiction",
+    "Good Omens": "Comedy"
+}
+
+book = input("select book: ")
+print(books.get(book))
