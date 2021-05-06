61f3f33641616f632f2eb8e73da191e423c51e35
kyrylo <data.mail.wild@gmail.com>
2021-05-06 18:24:55 +0200

    #practice with string functions

--- py_6/py_6.9.py
+++ py_6/py_6.9.py
@@ -1,5 +1,5 @@
 #practice with string functions
-print(", ".join(["qwerty", "qwerty"]))
+"""print(", ".join(["qwerty", "qwerty"]))
 print("hello me".replace("me","world"))
 print("qwerty qwerty".startswith("qwerty"))
 print("qwerty qwerty".endswith("qwerty"))
@@ -9,4 +9,7 @@ print("qwerty qwerty".split())
 print(min(1,2,3,4,5,6,7,8,9,0))
 print(max([1,2,3,4,5,6,7,8,9,0]))
 print(abs(-88))
-print(sum([1,2,3,4,5,6,7,8,9,0]))
+print(sum([1,2,3,4,5,6,7,8,9,0]))"""
+txt = input()
+print(txt.replace("#"," "))
+
