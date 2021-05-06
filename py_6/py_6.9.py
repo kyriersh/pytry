59ae5d67e0dac575c24232167cc51c38537071ab
kyrylo <data.mail.wild@gmail.com>
2021-05-06 18:12:21 +0200

    #practice with string functions

---
+++ py_6/py_6.9.py
@@ -0,0 +1,8 @@
+#practice with string functions
+print(", ".join(["qwerty", "qwerty"]))
+print("hello me".replace("me","world"))
+print("qwerty qwerty".startswith("qwerty"))
+print("qwerty qwerty".endswith("qwerty"))
+print("qwerty qwerty".upper())
+print("QWERTY QWERTY".lower())
+print("qwerty qwerty".split())
