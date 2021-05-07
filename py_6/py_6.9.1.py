c694ebc4ebbde273cd856074436a122ad2fb2fd4
kyrylo <data.mail.wild@gmail.com>
2021-05-07 17:58:23 +0200

    #practice with string functions

--- py_6/py_6.9.1.py
+++ py_6/py_6.9.1.py
@@ -9,3 +9,6 @@ filename = input(" enter filename : ")
 with open(filename) as f :
     text = f.read()
 print(count_char(text,"r"))
+for char in "qwertyuiopasdfghjklzxcvbnm":
+    perc = 100 * count_char(text, char)/len(text)
+    print("{0} - {1}%".format(char,round(perc,2)))
