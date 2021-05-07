62926ef14ec0c8794915db5cc585351a30382759
kyrylo <data.mail.wild@gmail.com>
2021-05-07 18:47:48 +0200

    password generator

--- key_gen.py
+++ key_gen.py
@@ -1,14 +1,13 @@
 # password generator
 import random
-n = int(input(" ssword length : "))
+n = int(input(" password length : "))
 def key_gen(lenghth):
     alphabet = 'qwertyuiopasdfghjklzxcvbnm' \
                'QWERTYUIOPASDFGHJKLZXCVBNM0123456789' \
                '/*-+!@#$%^&*()=\[]{}_?><'
     passw = ''
     for i in range(n):
-        symbol = random.choice(alphabet)
-        passw = passw + symbol
+        passw += random.choice(alphabet)
     return passw
 passw = key_gen(n)
 print(passw)
