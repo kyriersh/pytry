2a52d20b9960c973deb1ca389aef322afce32d93
kyrylo <data.mail.wild@gmail.com>
2021-05-12 11:22:23 +0200

    # practice with Warlus

--- py_1/py_1.2.py
+++ py_1/py_1.2.py
@@ -4,3 +4,7 @@ first_group = int(input(" Input kids in 1 group : "))
 second_group = int(input(" Input kids in 2 group : "))
 third_group = int(input(" Input kids in 3 group : "))
 print((first_group + second_group + third_group)*2)
+"""print(gr1:= int(input(" Input kids in 1 group : ")),
+      gr2:= int(input(" Input kids in 2 group : ")),
+      gr3:= int(input(" Input kids in 3 group : ")),
+      res:= ((gr1 + gr2 + gr3)*2))"""# practice with Warlus (:=)
