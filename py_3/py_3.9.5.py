119f202bed1247e6107bedb570e0d3ce7e88226e
kyrylo <data.mail.wild@gmail.com>
2021-05-14 19:27:26 +0200

    # just practice

--- py_3/py_3.9.5.py
+++ py_3/py_3.9.5.py
@@ -1,7 +1,7 @@
 # practice with continue
-a = int(input('start'))
-b = int(input('stop'))
-c = int(input('step'))
+a = int(input('start:'))
+b = int(input('stop:'))
+c = int(input('step:'))
 for x in range(a,b,c):
     if x % 2 == 0 :
         continue
