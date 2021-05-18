117dc4e7ecab71b91eca5aa0194282f1047fdb19
kyrylo <data.mail.wild@gmail.com>
2021-05-18 02:58:12 +0200

    count char in text and percentage

--- py_6/py_6.9.1.py
+++ py_6/py_6.9.1.py
@@ -1,4 +1,4 @@
-# practice with text
+# practice with text, count char in text and percentage
 def count_char(text, char):
     count = 0
     for i in text:
@@ -6,9 +6,10 @@ def count_char(text, char):
             count+=1
     return count
 filename = input(" enter filename : ")
+ch = input(" input char : ")
 with open(filename) as f :
     text = f.read()
-print(count_char(text,"r"))
+print(count_char(text, ch))
 for char in "qwertyuiopasdfghjklzxcvbnm":
     perc = 100 * count_char(text, char)/len(text)
     print("{0} - {1}%".format(char,round(perc,2)))
