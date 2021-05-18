1dc36c5d533d37f8cb5b827fcd0e91953f1524d5
kyrylo <data.mail.wild@gmail.com>
2021-05-18 02:59:04 +0200

    add different values

--- py_3/py_3.9.2.py
+++ py_3/py_3.9.2.py
@@ -1,4 +1,4 @@
 a = int(input())
 b = int(input())
-c = list(range(a, b, 1))
+c = list(range(a, b, -1))
 print(c)
--- py_6/py_6.7.py
+++ py_6/py_6.7.py
@@ -1,5 +1,5 @@
 # string formatting
-nums = list(range(5))
+nums = list(range(5,15,1))
 msg = "Numbers: {0} {1} {2} {3} {4}".format(nums[0],nums[1],nums[2],nums[3],nums[4])
 print(msg)
 print("{0}{1}{0}".format("abra","cad"))
