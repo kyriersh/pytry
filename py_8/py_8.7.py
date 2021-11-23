45be04894b8c17c2f4586996f1d18aa953490788
kyrylo <data.mail.wild@gmail.com>
2021-11-23 18:33:01 +0100

    # practice with oop

---
+++ py_8/py_8.7.py
@@ -0,0 +1,16 @@
+#practice oop
+class Number:
+    def __init__(self, num):
+        self.value = num
+
+    # your code goes here
+    @property
+    def isEven(self):
+        if self.value % 2 == 0:
+            return True
+        else:
+            return False
+
+
+x = Number(int(input()))
+print(x.isEven)
